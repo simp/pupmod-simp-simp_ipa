e31994718cc8137288d85729eea2cfd0681c7404
Nick Miller <nick.miller@onyxpoint.com>
2018-05-31 11:38:52 -0400

    (SIMP-4915) Add class to join IPA domains
    This commit adds a preliminary class for joining IPA domains with Puppet
    
    SIMP-4915 #close

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,21 +1,13 @@
 require 'spec_helper'
 
 describe 'simp_ipa' do
-  shared_examples_for "a structured module" do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to create_class('simp_ipa') }
-    it { is_expected.to contain_class('simp_ipa::install') }
-  end
-
-  context 'supported operating systems' do
-    on_supported_os.each do |os, os_facts|
-      context "on #{os}" do
-        let(:facts) do
-          os_facts
-        end
-
-        it_behaves_like "a structured module"
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) do
+        os_facts
       end
+
+      it { is_expected.to compile.with_all_deps }
     end
   end
 end
