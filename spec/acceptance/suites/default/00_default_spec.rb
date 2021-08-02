e99ed54d399ef37c72d31d12d8506a016bf7bbb9
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2021-08-02 18:20:19 -0400

    (SIMP-10405) simp_ipa advertises EL8 when unsupported (#29)
    * Fixed:
      - Drop advertised support for EL8, as `simp_ipa::client::install`
        has not been updated to integrate with the SIMP-managed PAM
        stack on EL8.
    * Not addressed
      * Did not change the default suite to actually do more than load
        the empty `simp_ipa` class.
        * simp_ipa::client::install is actually only tested in the 'ipa'
          suite in simp-core.
        * The 'join' and 'leave' tasks in this project are untested.
      * Did not remove EL8 from the nodesets in the acceptance tests.
    
    SIMP-10405 #close

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -4,6 +4,7 @@ test_name 'simp_ipa class'
 
 describe 'simp_ipa class' do
   let(:manifest) {
+    #FIXME This tests nothing. The simp_ipa class is empty.
     <<-EOS
       class { 'simp_ipa': }
     EOS
@@ -18,6 +19,13 @@ describe 'simp_ipa class' do
       it 'should be idempotent' do
         apply_manifest_on(host, manifest, :catch_changes => true)
       end
+
+      # simp_ipa::client::install is tested somewhat in the simp-core ipa suite
+      it 'should test simp_ipa::client::install'
+
+      # tasks not yet tested
+      it 'should test join task'
+      it 'should test leave task'
     end
   end
 end
